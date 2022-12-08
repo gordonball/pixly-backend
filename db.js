85d873f49d226fb579d3fa33c468ce8d85a74187
Gordon Ball <gball261@gmail.com>
2022-12-07 19:29:33 -0500

    database testing

--- db.js
+++ db.js
@@ -1,36 +1,36 @@
 "use strict";
 
+require("dotenv").config();
+console.log("env!!!", process.env.DATABASE);
 /** Database setup for pixly */
 
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(
-   process.env.DATABASE,
-   process.env.DATABASE_USER,
-   process.env.DATABASE_PASSWORD,
-   {
-     dialect: 'postgres',
-   },
- );
-async function testConnection(){
-try {
-   await sequelize.authenticate();
-   console.log('Connection has been established successfully.');
- } catch (error) {
-   console.error('Unable to connect to the database:', error);
- }
+const sequelize = new Sequelize({
+  database: process.env.DATABASE,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  dialect: "postgres",
+});
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 testConnection();
 
-const ImageModel = sequelize.define("image", {
-   image_url: { 
-      type: DataTypes.TEXT, 
-      allowNull: false,
-      primaryKey:true,
-    },
-   title: { type: DataTypes.TEXT, allowNull: false },
-   description: { type: DataTypes.TEXT, allowNull: false },
-   uploaded_by: { type: DataTypes.TEXT, allowNull: false },
+const ImageModel = sequelize.define("images", {
+  image_url: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: { type: DataTypes.TEXT, allowNull: false },
+  description: { type: DataTypes.TEXT, allowNull: false },
+  uploaded_by: { type: DataTypes.TEXT, allowNull: false },
 });
 
 module.exports = { sequelize, ImageModel };
